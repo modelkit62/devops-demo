64b50bf744c1913d92ffb21f0caefc69af88edc4
modelkit62 <modelkit62@gmail.com>
2020-12-20 20:51:19 +0000

    fixing runnning probs

--- src/main/java/com/devops/devopsdemo/HelloController.java
+++ src/main/java/com/devops/devopsdemo/HelloController.java
@@ -1,14 +1,19 @@
 package com.devops.devopsdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping(value="/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
+    @GetMapping(value="/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
